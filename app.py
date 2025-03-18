9626453cfd36d94b81ff7bdff344cfde6360c5b0
devsergiosantos <analista.sergiosantos@gmail.com>
2025-03-18 16:29:24 -0300

    app.py

--- app.py
+++ app.py
@@ -133,8 +133,19 @@ def main():
     if "logged_in" not in st.session_state:
         st.session_state["logged_in"] = False
         st.session_state["username"] = ""
-        
-    components.iframe("https://www.youtube.com/embed/liC49Dol6OY?si=2VsirqJsLrPdGi87&amp;controls=0" , width=400, height=400)
+         st.title("🎶 Bem-vindo ao Curso de Música – Fanfara! 🎺🥁")  
+
+st.write(  
+    """É com grande alegria que recebemos você nesta jornada musical!  
+    Aqui, você terá a oportunidade de aprender, evoluir e fazer parte de um grupo que valoriza a disciplina,  
+    a harmonia e o trabalho em equipe.  
+
+    Prepare-se para explorar novos sons, aprimorar suas habilidades e viver momentos inesquecíveis.  
+    Que a música seja nossa conexão e inspiração! 🎼  
+
+    **Bem-vindo à família da Fanfara!** 🎵"""  
+)
+    components.iframe("https://www.youtube.com/embed/liC49Dol6OY?si=2VsirqJsLrPdGi87&amp;controls=0" , width=700, height=400)
    
     # Mostrar nome do usuário logado na barra lateral
     if st.session_state["logged_in"]:
